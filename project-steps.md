4ddc08699d7ef8d125adea0cb29663bf84fd6644
Túlio Gomes <tuliogomes07@hotmail.com>
2026-08-10 14:57:48 -0300

    Add ExportController and ImportController with index methods, and update routes for import and export functionality

--- project-steps.md
+++ project-steps.md
@@ -7,3 +7,5 @@ php artisan make:model Club -a
 php artisan make:controller DashBoardController
 
 Para tratar e lidar com os dados de importação e exportação irei utilizar o pacote Laravel Excel https://laravel-excel.com/
+
+Criado controller de importação e exportação e já deixei criada a rota.
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\DashBoardController;
+use App\Http\Controllers\ExportController;
+use App\Http\Controllers\ImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
+
+    Route::post('/import', [ImportController::class, 'index'])->name('import');
+    Route::get('/export', [ExportController::class, 'index'])->name('export');
 });
 
 require __DIR__.'/settings.php';
